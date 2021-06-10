676641788fbedd2966bffe3cb7ab667271a788a8
Arungon <arun.g@cognitivesys.co.uk>
2021-06-10 17:47:29 +0530

    added the changes

---
+++ dist/js/app.js
@@ -0,0 +1,8 @@
+function lmenu() {
+    var lmenu = document.getElementById("lmenu");
+    lmenu.classList.toggle("lmenu-hide");
+}
+function rmenu() {
+    var rmenu = document.getElementById("rmenu");
+    rmenu.classList.toggle("rmenu-hide");
+}
